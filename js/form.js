1bdceaecfb8262e222b2ff65c6eabcbd01c96269
CodeforMood <strizhov.vladislav96@gmail.com>
2022-11-03 00:23:56 +0500

    Добавляет новые формы валидации

--- js/form.js
+++ js/form.js
@@ -1,9 +1,22 @@
 const MAX_LENGTH_TITLE = 100;
 const MIN_LENGTH_TITLE = 30;
 const MAX_VALUE_PRICE = 100000;
-const MIN_VALUE_PRICE = 0;
+const MIN_PRICES = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000,
+};
+const ROOMS = {
+  1: ['1'],
+  2: ['2', '1'],
+  3: ['3', '2' , '1'],
+  100: ['0'],
+};
 
 const offerForm = document.querySelector('.ad-form');
+
 const pristine = new Pristine( offerForm, {
   classTo: 'ad-form__element',
   errorClass: 'ad-form__element--invalid',
@@ -15,23 +28,26 @@ const validateTitle = (value) => value.length >= MIN_LENGTH_TITLE && value.lengt
 
 pristine.addValidator(offerForm.querySelector('#title'), validateTitle, `от ${MIN_LENGTH_TITLE} до ${MAX_LENGTH_TITLE} символов`);
 
-const validatePrice = (value) => value >= MIN_VALUE_PRICE && value <= MAX_VALUE_PRICE;
+const typeField = offerForm.querySelector('#type ');
+const priceField = offerForm.querySelector('#price');
 
-const getPriceErrorMessage = () => `Максимальная цена — ${MAX_VALUE_PRICE} руб, минимальная цена — ${MIN_VALUE_PRICE} руб`;
+const onTypeChange = () => {
+  priceField.placeholder = MIN_PRICES[typeField.value];
+  pristine.validate(priceField);
+};
+
+typeField.addEventListener('change',onTypeChange);
+
+const validatePrice = (value) => value >= MIN_PRICES[typeField.value] && value <= MAX_VALUE_PRICE;
+
+const getPriceErrorMessage = () => `Максимальная цена — ${MAX_VALUE_PRICE} руб, минимальная цена — ${MIN_PRICES[typeField.value]} руб`;
 
 pristine.addValidator(offerForm.querySelector('#price'), validatePrice, getPriceErrorMessage);
 
 const roomsField = offerForm.querySelector('#room_number');
 const guestsField = offerForm.querySelector('#capacity');
 
-const rooms = {
-  1: ['1'],
-  2: ['2', '1'],
-  3: ['3', '2' , '1'],
-  100: ['0'],
-};
-
-const validateRooms = () => rooms[roomsField.value].includes(guestsField.value);
+const validateRooms = () => ROOMS[roomsField.value].includes(guestsField.value);
 
 const getRoomsErrorMessage = () => {
   switch (roomsField.value) {
@@ -50,6 +66,20 @@ const getRoomsErrorMessage = () => {
 };
 
 pristine.addValidator(roomsField, validateRooms, getRoomsErrorMessage);
+
+const checkFieldset = offerForm.querySelector('.ad-form__element--time');
+const checkinField = offerForm.querySelector('#timein');
+const checkoutField = offerForm.querySelector('#timeout');
+
+const onCheckChange = (evt) => {
+  if (evt.target.id === 'timein') {
+    checkoutField.value = checkinField.value;
+  } else {checkinField.value = checkoutField.value;
+  }
+};
+
+checkFieldset.addEventListener('change', onCheckChange);
+
 
 offerForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
